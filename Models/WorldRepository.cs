79502e553190c111fad6667f518a0199c01b9116
maenpsam <sami.maenpaa@welho.com>
2016-08-31 12:25:41 +0300

    Eka versio

---
+++ Models/WorldRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Models;
+
+namespace TheWorld.Models
+{
+    public class WorldRepository : IWorldRepository
+    {
+        private WorldContext _context;
+        private ILogger<WorldRepository> _logger;
+
+        public WorldRepository(WorldContext context, ILogger<WorldRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public void AddStop(string tripName, string username, Stop newStop)
+        {
+            var trip = GetTripByName(tripName, username);
+            if (trip != null)
+            {
+                if (trip.Stops.Count > 0)
+                {
+                    newStop.Order = trip.Stops.Max(s => s.Order) + 1;
+                }
+                trip.Stops.Add(newStop);
+                _context.Stops.Add(newStop);
+            }
+        }
+
+        public void AddTrip(Trip trip)
+        {
+            _context.Add(trip);
+        }
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            try
+            {
+                return _context.Trips.OrderBy(t => t.Name).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not get trips from the database");
+                return null;
+            }
+        }
+
+        public Trip GetTripByName(string tripName, string username)
+        {
+            return _context.Trips
+                .Include(t => t.Stops)
+                .Where(trip => trip.Name == tripName && trip.UserName == username)
+                .FirstOrDefault();
+        }
+
+        public IEnumerable<Trip> GetUserTripsWithStops(string name)
+        {
+            try
+            {
+                return _context.Trips
+                .Include(t => t.Stops)
+                .OrderBy(t => t.Name)
+                .Where(t => t.UserName == name)
+                .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Could not get trips with stops from database", ex);
+                return null;
+            }
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+           return (await _context.SaveChangesAsync()) > 0;
+        }
+    }
+}
